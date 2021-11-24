fdba82820a4957f87c1d8588def38c9238eb91e1
pfwd <hello@websomatic.co.uk>
2021-11-24 19:17:46 +0000

    Removing images

--- project/config/orderConfig.php
+++ project/config/orderConfig.php
@@ -7,25 +7,21 @@ return [
             [
                 'product_name' => 'Keyboard',
                 'product_price' => 89.99,
-                'product_image' => 'keyboard.jpg',
                 'order_date' => '01-02-2021',
             ],
             [
                 'product_name' => 'Mouse',
                 'product_price' => 99.99,
-                'product_image' => 'mouse.jpg',
                 'order_date' => '01-03-2021',
             ],
             [
                 'product_name' => 'Monitor',
                 'product_price' => 1500.99,
-                'product_image' => 'monitor.jpg',
                 'order_date' => '01-04-2021',
             ],
             [
                 'product_name' => 'Mixing deck',
                 'product_price' => 1500.99,
-                'product_image' => 'mixing_deck.jpg',
                 'order_date' => '01-04-2021',
             ],
         ]
@@ -36,7 +32,6 @@ return [
             [
                 'product_name' => 'Watch',
                 'product_price' => 350,
-                'product_image' => 'watch.jpg',
                 'order_date' => '01-02-2021',
             ],
         ]
